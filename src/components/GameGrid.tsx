5d467cb6a88984dcf7fa82f431895329b43a4de7
HoussamMrabet <dodslim100@gmail.com>
2023-06-18 13:27:36 +0100

    clean up games cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,7 +17,7 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={3}
         paddingY="10px"
       >
