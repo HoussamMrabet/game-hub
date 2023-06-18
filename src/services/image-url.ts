01a01c3943f78e3c28e4acb68ce24ce2464f5db4
HoussamMrabet <dodslim100@gmail.com>
2023-06-18 12:36:29 +0100

    handle no image game api issue

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeholderImage from "../assets/placeholder-image.jpg";
+
 const getCroppedImageUrl = (url: string) => {
-    if(!url) return null;
+    if(!url) return placeholderImage;
     const target ='media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
